37d306e0e8f2935383c67e140c62b85ec6d4815f
Mark Meany <mark.meany@os.uk>
2016-12-19 10:42:24 +0000

    Initial

---
+++ src/main/java/os/uk/SonarPlayApplication.java
@@ -0,0 +1,12 @@
+package os.uk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarPlayApplication.class, args);
+	}
+}
---
+++ src/test/java/os/uk/SonarPlayApplicationTests.java
@@ -0,0 +1,16 @@
+package os.uk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SonarPlayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
